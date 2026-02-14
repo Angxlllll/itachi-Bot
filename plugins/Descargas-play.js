a9a6f0c0bb36062553f0f773494103847a2327ac
GABRIEL-OFF18 <virginiarimachipalmares@gmail.com>
2026-02-14 10:10:24 -0500

    Actualizar Descargas-play.js

--- plugins/Descargas-play.js
+++ plugins/Descargas-play.js
@@ -44,13 +44,9 @@ let handler = async (m, { conn, text, usedPrefix }) => {
     }
 
     // ===== APIs para audio MP3 =====
-    const fuentes = [
-      { api: 'ZenzzXD', endpoint: `https://api.zenzxz.my.id/downloader/ytmp3?url=${encodeURIComponent(url)}`, extractor: res => res.download_url },
-      { api: 'ZenzzXD v2', endpoint: `https://api.zenzxz.my.id/downloader/ytmp3v2?url=${encodeURIComponent(url)}`, extractor: res => res.download_url },
-      { api: 'Vreden', endpoint: `https://api.vreden.my.id/api/ytmp3?url=${encodeURIComponent(url)}`, extractor: res => res.result?.download?.url },
-      { api: 'Delirius', endpoint: `https://api.delirius.my.id/download/ymp3?url=${encodeURIComponent(url)}`, extractor: res => res.data?.download?.url },
-      { api: 'StarVoid', endpoint: `https://api.starvoidclub.xyz/download/youtube?url=${encodeURIComponent(url)}`, extractor: res => res.audio }
-    ]
+     const apiUrl = type === "mp3"
+      ? `https://api-adonix.ultraplus.click/download/ytaudio?url=${encodeURIComponent(url)}&apikey=WilkerKeydukz9l6871`
+      : `https://api-adonix.ultraplus.click/download/ytvideo?url=${encodeURIComponent(url)}&apikey=WilkerKeydukz9l6871`
 
     let audioUrl, apiUsada, exito = false
 
